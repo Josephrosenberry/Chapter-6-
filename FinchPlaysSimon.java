160e735cd41a8e2249afaae933252fe58a6295a3
Josephrosenberry <31011380+Josephrosenberry@users.noreply.github.com>
2017-12-04 09:12:45 -0800

    Add files via upload

---
+++ FinchPlaysSimon.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class FinchPlaysSimon{
+    public static void main (String [] args){
+        int temp = (int) (Math.random() * 4);
+        ArrayList <String> Direction = new ArrayList<String>();
+        ArrayList Direction
+        if (  temp == 0)
+            System.out.println("Finch move: Up");
+        else if ( temp == 1)
+            System.out.println("Finch move: Down");
+        else if (  temp == 2)
+            System.out.println("Finch move: left");
+        else if ( (int) temp == 3)
+            System.out.println("Finch move: right");
+
+        
+    }
+}
